cadc47bf14c76ee620f05f3d95dbf5ea55118dfd
Mr-siva <119104724+Mr-siva@users.noreply.github.com>
2023-12-12 12:32:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,12 +7,12 @@ variable "ami" {
     default = "ami-02a2af70a66af6dfb"
   
 }
-
+/*
 variable "name" {
     dafault = "siva"
 }
 variable "created_by" {
     dafault = "terraform_git_modules"
 }
-
+*/
 
